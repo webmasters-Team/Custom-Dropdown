6e30f33de72bc1d33bb929ea46166ed98b878e10
webmasters <webmasters096@gmail.com>
2024-12-26 09:06:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(".dropdown li").on('mouseenter mouseleave', function (e) {
+        if ($('ul', this).length) {
+            var elm = $('ul:first', this);
+            var off = elm.offset();
+            var l = off.left;
+            var w = elm.width();
+            var docH = $(".container").height();
+            var docW = $(".container").width();
+
+            var isEntirelyVisible = (l + w <= docW);
+
+            if (!isEntirelyVisible) {
+                $(this).addClass('edge');
+            } else {
+                $(this).removeClass('edge');
+            }
+        }
+    });
+});
